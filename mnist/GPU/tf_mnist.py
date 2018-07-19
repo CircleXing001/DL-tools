95999fbfb2726b663edbbd515b3441682cdad206
CircleXing001 <30278923+CircleXing001@users.noreply.github.com>
2018-07-20 02:20:02 +0800

    Update tf_mnist.py

--- mnist/GPU/tf_mnist.py
+++ mnist/GPU/tf_mnist.py
@@ -1,4 +1,4 @@
-import time
+import time 
 import tensorflow as tf 
 import tensorflow.examples.tutorials.mnist.input_data as input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)     
